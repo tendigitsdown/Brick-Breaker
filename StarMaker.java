ef9caf42707f56bd7c06ca46e3c88c3014fb6a77
Sachin Goel <sachingoel@gmail.com>
2011-02-13 21:43:41 -0800

    Added StarMaker -- onClick displays coordinates in a GLabel

---
+++ StarMaker.java
@@ -0,0 +1,27 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * This program creates a five-pointed start every time the user
+ * clicks the mouse on the canvas.
+ * @author sachingoel
+ */
+
+public class StarMaker extends GraphicsProgram {
+
+/* Initializes the mouse listeners */
+	public void init() {
+		addMouseListeners();
+	}
+	
+/* Called whenever the user clicks the mouse */
+	public void mouseClicked(MouseEvent e) {
+		GLabel coords = new GLabel("(" + e.getX() + ", " + e.getY() + ")");
+		add(coords, e.getX(), e.getY());
+	}
+/* Private constants */
+	private static final int STAR_SIZE = 20;
+	
+}
